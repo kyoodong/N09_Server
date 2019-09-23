3bd9cc74c053150b7c2b5fe4744be1bb77ab3b64
Dongkyoo <wind.dong.dream@gmail.com>
2019-09-23 22:29:55 +0900

    기본 폼 완성

---
+++ frontend/src/Tab.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-tab :title="title" active>
+    <b-table-simple hover small>
+      <b-thead>
+        <b-tr>
+          <b-th>이름</b-th>
+          <b-th>url</b-th>
+          <b-th>옵션</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr v-for="(data, index) in urlList">
+          <b-td>{{ data.name }}</b-td>
+          <b-td>{{ data.url }}</b-td>
+          <b-td>
+            <b-button @click="delete(index)" variant="danger">삭제</b-button>
+            <b-button @click="modify(index)">수정</b-button>
+          </b-td>
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
+  </b-tab>
+</template>
+
+<script>
+export default {
+  name: 'tab',
+  props: ['title', 'urlList'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+    delete(index) {
+
+    },
+    modify(index) {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
