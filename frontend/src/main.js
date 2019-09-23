6c77fee099e7c351a1720559e282b351d5a7227e
Dongkyoo <wind.dong.dream@gmail.com>
2019-09-23 22:10:39 +0900

    Vue router 설치

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
-import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Main from './Main.vue'
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/', component: Main
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  router
+}).$mount('#app')
